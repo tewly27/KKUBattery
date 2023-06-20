0938359026a2e339d4228c4d6566548b5b0062ac
Supakorn <tewly27@gmail.com>
2023-06-20 15:25:59 +0700

    no message

--- qr.py
+++ qr.py
@@ -1,4 +1,4 @@
 import qrcode
 imei = '0868755123661050'
-img = qrcode.make('https://uvolt360.kku.ac.th/#'+imei)
+img = qrcode.make('https://iqw3pm3mgm.ap-southeast-2.awsapprunner.com/#0868755123661050')
 img.save(imei+".png")
